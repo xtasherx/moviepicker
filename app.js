e0c69d0810140c10e46609c2f1a3485c739446d3
Natasha Harrington <xtasherx@gmail.com>
2019-06-05 00:33:27 -0500

    app.js - updated vars to let/const style.css - updated padding in .add-show

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-var shows = ['westworld', 'battlestar gallactica', 'Star Trek Discovery', 'avatar:the last airbender', 'chernobyl',
-  'aggretsuko', 'eureka', 'z-nation', 'warehouse 13'
+const shows = [
+  'westworld',
+  'battlestar gallactica',
+  'Star Trek Discovery',
+  'avatar:the last airbender',
+  'chernobyl',
+  'aggretsuko',
+  'eureka',
+  'z-nation',
+  'warehouse 13'
 ];
-var answerP = document.querySelector('.answer');
-var randomizeButton = document.querySelector('.random-btn');
-var addButton = document.querySelector('.add-btn');
-var addShow = document.querySelector('.add-show');
-var viewList = document.querySelector('.view-btn');
-var ul = document.querySelector(".show-list");
-var listItem = document.createElement('li');
-var removeButtonAction = document.querySelector('.remove-btn');
-var removeButton = '<input type="button" class="remove-btn" value="remove"></input>';
-var visibleList = true;
+
+const answerP = document.querySelector('.answer');
+const randomizeButton = document.querySelector('.random-btn');
+const addButton = document.querySelector('.add-btn');
+const addShow = document.querySelector('.add-show');
+const viewList = document.querySelector('.view-btn');
+const ul = document.querySelector(".show-list");
+let listItem = document.createElement('li');
+const removeButtonAction = document.querySelector('.remove-btn');
+let removeButton = '<input type="button" class="remove-btn" value="remove"></input>';
+let visibleList = true;
 
 // function to pick random show from array
 function pickAShow() {
-  var randomShow = shows[Math.floor(Math.random() * shows.length)];
+  const randomShow = shows[Math.floor(Math.random() * shows.length)];
   return randomShow.toLowerCase();
 }
 
 // function to print array intms into ul
 function arrayPrint() {
-  for (var i = 0; i < shows.length; i++) {
-    var listItem = document.createElement('li');
+  for (let i = 0; i < shows.length; i++) {
+    const listItem = document.createElement('li');
     listItem.innerHTML = removeButton + "  " + shows[i];
     ul.appendChild(listItem);
   }
@@ -37,13 +46,17 @@ randomizeButton.addEventListener("click", function() {
 
 // on click push addShow answer to array and print new li to page.
 addButton.addEventListener('click', function() {
-  var pushShow = addShow.value;
+  let pushShow = addShow.value;
+  if(addShow.value === ''){
+
+  }else{
   shows.push(pushShow);
   listItem = document.createElement('li');
   listItem.innerHTML = removeButton + '  ' + addShow.value;
   ul.appendChild(listItem);
   console.log(pushShow);
   addShow.value = "";
+}
 });
 
 // show-hide list on button click
@@ -59,8 +72,13 @@ viewList.addEventListener('click', function() {
 })
 
 // ************* TO DO *********************************
+
 // remove items from array and ul list on button click
-// figure out a way to not submit empty strings to array
+
+// removeButtonAction.addEventListener('click', function(){
+//
+// });
+
 // possible toggle for list instead of button?
 // add event listener for return button press
 // create the <p> for the random show with JS so there isn't a space taken up before population
